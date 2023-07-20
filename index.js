62df48ce1ecf6f3130decc2a08099a4e50b771ba
Galangyp <galangy11@gmail.com>
2023-07-20 17:49:05 +0700

    ubah db

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const port = 3306
+const port = 3306;
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`runnig on port ${port}`);
-})
+  console.log(`runnig on port ${port}`);
+});
 
 const db = mysql.createPool({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12610237',
-    password: 'FmhZhjDfSc',
-    database: 'sql12610237'
-})
+  host: "sql6.freesqldatabase.com",
+  user: "sql6634025",
+  password: "V63VtmDpwY",
+  database: "sql6634025",
+});
 
-app.use(cors());    
+app.use(cors());
 
-app.get("/", (req,res) => {
-    
-    const sqlQuery = "SELECT * FROM dongeng";
-    db.query(sqlQuery, (err, rows) => {
+app.get("/", (req, res) => {
+  const sqlQuery = "SELECT * FROM dongeng";
+  db.query(sqlQuery, (err, rows) => {
+    if (err) {
+      res.send("ERROR DATABASE");
+    } else {
+      res.json(rows);
+    }
+  });
+});
 
-        if (err) {
-            res.send("ERROR DATABASE")       
-        } else {
-            res.json(rows)
-        }
-              
-    })
-    
-})
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
 
-app.get("/:id", (req,res) => {
-    const id = req.params.id;
-    
-    const sqlQuery = `SELECT * FROM dongeng where id_dongeng = ${id}`;
-    db.query(sqlQuery, (err, rows) => {
-        
-        try{
-            res.json(rows[0])     
-        } catch (error) {
-            res.json({ message: error.message});
-        }
-     
-    })
-    
-})
-
+  const sqlQuery = `SELECT * FROM dongeng where id_dongeng = ${id}`;
+  db.query(sqlQuery, (err, rows) => {
+    try {
+      res.json(rows[0]);
+    } catch (error) {
+      res.json({ message: error.message });
+    }
+  });
+});
